c1f9a81126bfa9c655abdfd1d2e11f95ac5c2f3b
Sam Detweiler <sdetweil@gmail.com>
2019-05-08 14:02:38 -0500

    fix typoes, etc... add some vidoes

--- node_helper.js
+++ node_helper.js
@@ -5,68 +5,91 @@ var self;
 
 module.exports = NodeHelper.create ({
 	// data defaults
-	defaults: {
-	videoDir:"videos",
-	autoplay:"false",
-	loop:"false",
-	},
-	self:0,
+	//defaults: {
+	//videoDir:"videos",
+	//autoplay:"false",
+	//loop:"false",
+	//},
+	//self:0,
 	
 	// make Promise version of fs.readdir()
-    readdirAsync : function(dirname) {
-      return new Promise(function(resolve, reject) {
-	 fs.readdir(dirname, function(err, filenames){
-	    if (err) 
-	        reject(err); 
-	    else 
-	        resolve(filenames);
-	 });
+  /*  readdirAsync : function(dirname) {
+      console.log("in readdir_async, source="+dirname)
+    return new Promise(function(resolve, reject) {
+      console.log("in readdir_async promise")
+         fs.readdir(dirname, function(err, filenames){
+            if (err) {
+                console.log(" read async error="+err)
+                reject(err); 
+            }
+            else {
+                 console.log(" readdir async all ok filenames.length="+ filenames.length )                 
+                resolve(filenames);
+            }
+         });
       });
-    },
+    }, */
 	
 	// function from here on
 	start: function() {
-	self=this;
+    self=this;
 		console.log("Starting module: " + this.name);
 	},
 	
 	getVideos: function(){
-	try {
-	   var files=fs.readdirSync(self.config.videoDir)
-	return files;
-	}
-	catch(exception) {
-		console.log("videoDir error ="+exception.messages);
-	}
+    console.log("getVideos path="+self.config.videoDir)
+    try {
+       // using async version of readdir, returns from promise
+       fs.readdir(self.config.videoDir, function(error,files){
+          if(error==null){
+            console.log("getVideos list="+self.config.videoDir + " list="+ JSON.stringify(files))
+            self.sendSocketNotification("Videos",files);
+          }
+          else{
+            console.log(" readdir error="+error)
+          }
+       })
+    }
+    catch(exception) {
+      console.log("videoDir error ="+exception.messages);
+    }
 	},
 	getAutoPlay: function() {
-	try {
-	   var autoplay = "videos";
-	   if (this.config.videoDir && this.config.videoDir.length > 0) {
-		   videoDir = "&videos=";
-		   for (var i = 0; i < this.config.videoDir.length; i++) {
-			   videoDir += this.config.[i];
-			   if (i + 1 < this.config.videoDir.length)
-				   videoDir += "&videos=";
-		   }   
-	}
+    try {
+       var autoplay = "videos";
+       if (this.config.videoDir && this.config.videoDir.length > 0) {
+         videoDir = "&videos=";
+         for (var i = 0; i < this.config.videoDir.length; i++) {
+           //videoDir += this.config.[i];
+           if (i + 1 < this.config.videoDir.length)
+             videoDir += "&videos=";
+         }   
+       }
+    }
+    catch(ex){
+      console.log("caught error="+ex.Message)
+    }
 	},
 	getLoop: function() {
-	try {
-	   var loop += (this.config.loop) ? "&loop=1: "&videos=";
-	}
+    try {
+       var loop = loop + (this.config.loop) ? "&loop=1": "&videos=";
+    }
+    catch(ex){
+      console.log("caught error="+ex.Message)
+    }
 	},
 	
 	socketNotificationReceived: function(notification, payload) {
 	if (notification === 'CONFIG') {
-	    this.config = payload;
+	   this.config = payload;
 	   console.log("config.payload received");
-	   self.config.videoDir=path.join(_dirname.toString(),self.config.videoDir);
+	   self.config.videoDir=path.join(this.path,self.config.videoDir);
+     console.log(" video path="+self.config.videoDir)
 	   self.config.autoplay= (this.config.autoplay) ? "autoplay=1" : "autoplay=0";
 	   self.config.loop= (this.config.loop) ? "&loop=1" : "videos";
 	}
 	else if (notification === 'GET_VIDEOS') {
-		self.sendSocketNotification("Videos",self.getVideos());
+		self.getVideos();
 	}
 	else if (notification === 'GET_AUTOPLAY') {
 		self.sendSocketNotification("Autoplay",self.getAutoPlay());
